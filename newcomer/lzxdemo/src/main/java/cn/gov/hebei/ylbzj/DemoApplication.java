9396f592bbb8675288e7f8e01e98169be08c2758
刘小刘 <85105451@qq.com>
2020-07-08 22:40:18 +0800

    +

--- newcomer/lzxdemo/src/main/java/cn/gov/hebei/ylbzj/DemoApplication.java
+++ newcomer/lzxdemo/src/main/java/cn/gov/hebei/ylbzj/DemoApplication.java
@@ -14,6 +14,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class DemoApplication {
 
     public static void main(String[] args) {
+
+        System.out.println(11111);
         SpringApplication.run(DemoApplication.class, args);
     }
 
